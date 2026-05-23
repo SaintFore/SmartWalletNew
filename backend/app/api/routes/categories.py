467bb58bf142b88e9079f7f52c6d4f74fca778dc
julien <forevergoodforest@outlook.com>
2026-05-23 19:43:20 +0800

    feat: add category services and router

---
+++ backend/app/api/routes/categories.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
+
+from app.db.session import get_session
+from app.models.category import Category
+from app.schemas.category import CategoryCreate, CategoryRead, CategoryUpdate
+from app.services.category_service import create, delete, get, get_all, update
+
+router = APIRouter(prefix="/categories", tags=["categories"])
+
+
+@router.get("", response_model=list[CategoryRead])
+def read_categorys(session: Annotated[Session, Depends(get_session)]) -> list[Category]:
+    return get(session)
+
+
+@router.get("/{category_id}", response_model=CategoryRead)
+def read_category(
+    item_id: int, session: Annotated[Session, Depends(get_session)]
+) -> Category:
+    category = get_all(session, item_id)
+    if not category:
+        raise HTTPException(status_code=404, detail="category not found")
+    return category
+
+
+@router.post("", response_model=CategoryRead)
+def create_new_category(
+    category_in: CategoryCreate,
+    session: Annotated[Session, Depends(get_session)],
+) -> Category:
+    return create(category_in, session)
+
+
+@router.delete("/{category_id}", status_code=204)
+def delete_n_category(item_id: int, session: Annotated[Session, Depends(get_session)]):
+    category = delete(item_id, session)
+    if not category:
+        raise HTTPException(status_code=404, detail="category not found")
+
+
+@router.patch("/{category_id}", response_model=CategoryRead)
+def update_n_category(
+    category_id: int,
+    item_in: CategoryUpdate,
+    session: Annotated[Session, Depends(get_session)],
+) -> Category:
+    category = update(category_id, item_in, session)
+    if not category:
+        raise HTTPException(status_code=404, detail="category not found")
+    return category
---
+++ backend/app/services/category_service.py
@@ -0,0 +1,49 @@
+from sqlmodel import Session, select
+
+from app.models.category import Category
+from app.schemas.category import CategoryCreate, CategoryUpdate
+
+
+def get_all(session: Session, category_id: int) -> Category | None:
+    return session.get(Category, category_id)
+
+
+def get(session: Session) -> list[Category]:
+    statement = select(Category)
+    return list(session.exec(statement).all())
+
+
+def create(category_in: CategoryCreate, session: Session) -> Category:
+    category = Category.model_validate(category_in)
+    session.add(category)
+    session.commit()
+    session.refresh(category)
+    return category
+
+
+def delete(category_id: int, session: Session) -> Category | None:
+    category = session.get(Category, category_id)
+
+    if category is None:
+        return None
+    session.delete(category)
+    session.commit()
+    return category
+
+
+def update(
+    category_id: int, category_in: CategoryUpdate, session: Session
+) -> Category | None:
+    category = session.get(Category, category_id)
+
+    if category is None:
+        return None
+
+    update_data = category_in.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(category, key, value)
+    session.add(category)
+    session.commit()
+    session.refresh(category)
+    return category
